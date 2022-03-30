f5e51972284a658cda32e601b812426391262a27
panhaoyu <panhaoyu.china@outlook.com>
2022-03-30 16:38:01 +0800

    开始开发

--- word/views.py
+++ word/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from phy_django.views import TemplateView
+
 from .models import WordModel
 
 
-# Create your views here.
-def index(request):
-    word_list = request.session.get('word_list', [])
-    word_list = [WordModel.objects.get(id=word) for word in word_list]
-    if not word_list:
-        word_list = WordModel.get_random(10)
-        request.session['word_list'] = [word.id for word in word_list]
-    return render(request, 'word/index.html', context={
-        'words': word_list,
-    })
+class BaseView(TemplateView):
+    site_name = 'Remember Me'
+
+
+class IndexView(BaseView):
+    page_title = '首页'
+    page_description = 'Remember Me 的首页'
+    template_name = 'word/index.html'
+
+    def get_context_data(self, **kwargs):
+        word_list = self.request.session.get('word_list', [])
+        word_list = [WordModel.objects.get(id=word) for word in word_list]
+        if not word_list:
+            word_list = WordModel.get_random(10)
+            self.request.session['word_list'] = [word.id for word in word_list]
+        kwargs |= {'words': word_list}
+        return kwargs
