4f948e9bf9466c9449075b116d3437e8c9406bd0
panhaoyu <panhaoyu.china@outlook.com>
2022-03-30 16:53:28 +0800

    建立后端渲染的架构

--- word/views.py
+++ word/views.py
@@ -13,10 +13,13 @@ class IndexView(BaseView):
     template_name = 'word/index.html'
 
     def get_context_data(self, **kwargs):
+        kwargs = super().get_context_data(**kwargs)
+
         word_list = self.request.session.get('word_list', [])
         word_list = [WordModel.objects.get(id=word) for word in word_list]
         if not word_list:
             word_list = WordModel.get_random(10)
             self.request.session['word_list'] = [word.id for word in word_list]
         kwargs |= {'words': word_list}
+
         return kwargs
