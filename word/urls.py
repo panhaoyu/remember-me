fd39526a13f4a7bf918d6033fde1b8941f534d75
panhaoyu <panhaoyu.china@outlook.com>
2022-04-03 23:20:02 +0800

    支持对媒体文件的路由

--- word/urls.py
+++ word/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -5,3 +7,5 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
